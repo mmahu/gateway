d99382144243c586f17e429e7bc5aea319c0b2a3
Mihail Mahu <mihailmahu@gmail.com>
2019-11-17 21:34:19 -0800

    'f'

---
+++ src/test/java/com/mmahu/gateway/MmahuGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmahu.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmahuGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
