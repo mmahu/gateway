b2f5b2a0c3dd8667701548c6a2c976196d965e61
Mihail Mahu <mihailmahu@gmail.com>
2019-11-23 08:55:39 -0800

    updated dependence

--- src/main/java/com/mmahu/gateway/configs/SecurityConfig.java
+++ src/main/java/com/mmahu/gateway/configs/SecurityConfig.java
@@ -1,27 +1,8 @@
 package com.mmahu.gateway.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfig {
-
-    @Bean
-    public MapReactiveUserDetailsService userDetailsService() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserDetails buyer = User.withUsername("buyer").password(encoder.encode("buyer")).roles("USER").build();
-        UserDetails seller = User.withUsername("seller").password(encoder.encode("seller")).roles("USER").build();
-        return new MapReactiveUserDetailsService(buyer, seller);
-    }
 
 }
