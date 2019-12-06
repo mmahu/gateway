fef54190f0e1338742aa0faa624c1b6a3bce1feb
Mihail Mahu <mihailmahu@gmail.com>
2019-12-05 22:48:13 -0800

    added security configs

--- src/main/java/com/mmahu/gateway/configs/SecurityConfig.java
+++ src/main/java/com/mmahu/gateway/configs/SecurityConfig.java
@@ -1,7 +1,50 @@
 package com.mmahu.gateway.configs;
 
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.security.Principal;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Bean
+    public GlobalFilter customGlobalFilter() {
+        return (exchange, chain) -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .map(userName -> {
+                    exchange.getRequest().mutate().header("Authorization", userName).build();
+                    return exchange;
+                })
+                .flatMap(chain::filter);
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
 }
