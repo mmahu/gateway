023f3f77448dcfe3251e2bb969e6161ca510206c
Mihail Mahu <mihailmahu@gmail.com>
2019-12-07 11:35:21 -0800

    updated with roles

--- src/main/java/com/mmahu/gateway/configs/SecurityConfig.java
+++ src/main/java/com/mmahu/gateway/configs/SecurityConfig.java
@@ -1,51 +1,74 @@
 package com.mmahu.gateway.configs;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mmahu.gateway.dto.JwtDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.jwt.Jwt;
 import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.jwt.crypto.sign.MacSigner;
-import org.springframework.security.jwt.crypto.sign.RsaSigner;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
 
-import java.security.Principal;
+import java.time.Instant;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
+import static java.util.Objects.nonNull;
 import static org.springframework.security.config.Customizer.withDefaults;
-import static org.springframework.security.config.web.server.SecurityWebFiltersOrder.AUTHENTICATION;
-import static org.springframework.security.config.web.server.SecurityWebFiltersOrder.AUTHORIZATION;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-
     @Value("${spring.security.oauth2.resource.jwt.key-value}")
-    String key;
+    private String key;
 
     @Bean
     public GlobalFilter customGlobalFilter() {
         return (exchange, chain) -> exchange.getPrincipal()
-                .map(Principal::getName)
-                .map(userName -> {
-                    exchange.getRequest().mutate().header("Authorization", toToken(userName)).build();
+                .filter(UsernamePasswordAuthenticationToken.class::isInstance)
+                .map(UsernamePasswordAuthenticationToken.class::cast)
+                .map(principal -> {
+                    String token = convert(principal);
+                    exchange.getRequest().mutate().header("Authorization", token).build();
                     return exchange;
                 }).flatMap(chain::filter);
     }
 
-    private String toToken(String userId) {
-        String token = JwtHelper.encode("{\"sub\": \"" + userId +  "\"}", new MacSigner(key)).getEncoded();
+    private String convert(UsernamePasswordAuthenticationToken principal) {
+        String authorities = toAuthorities(principal.getAuthorities());
+        JwtDto dto = new JwtDto()
+                .setSub(principal.getName())
+                .setScp(authorities)
+                .setIat(Instant.now())
+                .setExp(Instant.now().plusSeconds(60 * 30));
+        String content = toJson(dto);
+        String token = JwtHelper.encode(content, new MacSigner(key)).getEncoded();
         return "Bearer " + token;
+    }
+
+    private String toAuthorities(Collection<GrantedAuthority> authorities) {
+        if (nonNull(authorities)) {
+            return authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
+        }
+        return null;
+    }
+
+    private String toJson(JwtDto dto) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(dto);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Cannot convert JwtDto to Json");
+        }
     }
 
     @Bean
@@ -58,25 +81,12 @@ public class SecurityConfig {
         return new MapReactiveUserDetailsService(user);
     }
 
-//    @Bean
-//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-//        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
-//                .httpBasic(withDefaults())
-//                .formLogin(withDefaults());
-//        return http
-//                .addFilterAfter(new WebFilter(){
-//                    @Override
-//                    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-//                        exchange.getPrincipal()
-//                                .map(Principal::getName)
-//                                .map(userName -> {
-//                                    exchange.getRequest().mutate().header("Authorization", toToken(userName)).build();
-//                                    return exchange;
-//                                }).flatMap(chain::filter);
-//                        return chain.filter(exchange);
-//                    }
-//                }, AUTHORIZATION)
-//                .build();
-//    }
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+        return http.build();
+    }
 
 }
---
+++ src/main/java/com/mmahu/gateway/dto/JwtDto.java
@@ -0,0 +1,46 @@
+package com.mmahu.gateway.dto;
+
+import java.time.Instant;
+
+public class JwtDto {
+    private String sub;
+    private String scp;
+    private Instant iat;
+    private Instant exp;
+
+    public String getSub() {
+        return sub;
+    }
+
+    public JwtDto setSub(String sub) {
+        this.sub = sub;
+        return this;
+    }
+
+    public String getScp() {
+        return scp;
+    }
+
+    public JwtDto setScp(String scp) {
+        this.scp = scp;
+        return this;
+    }
+
+    public Instant getIat() {
+        return iat;
+    }
+
+    public JwtDto setIat(Instant iat) {
+        this.iat = iat;
+        return this;
+    }
+
+    public Instant getExp() {
+        return exp;
+    }
+
+    public JwtDto setExp(Instant exp) {
+        this.exp = exp;
+        return this;
+    }
+}
