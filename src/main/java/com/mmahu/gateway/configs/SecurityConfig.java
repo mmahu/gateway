1cb291c24963f576eb995c54883f46545920394d
Mihail Mahu <mihailmahu@gmail.com>
2019-12-06 18:50:19 -0800

    updated security configs

--- src/main/java/com/mmahu/gateway/configs/SecurityConfig.java
+++ src/main/java/com/mmahu/gateway/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.mmahu.gateway.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,9 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.jwt.crypto.sign.RsaSigner;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import java.security.Principal;
@@ -18,23 +22,30 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+    @Value("${spring.security.oauth2.resource.jwt.key-value}")
+    String key;
+
     @Bean
     public GlobalFilter customGlobalFilter() {
         return (exchange, chain) -> exchange.getPrincipal()
                 .map(Principal::getName)
                 .map(userName -> {
-                    exchange.getRequest().mutate().header("Authorization", userName).build();
+                    exchange.getRequest().mutate().header("Authorization", toToken(userName)).build();
                     return exchange;
-                })
-                .flatMap(chain::filter);
+                }).flatMap(chain::filter);
+    }
+
+    private String toToken(String userId) {
+        Jwt encode = JwtHelper.encode(userId, new RsaSigner(key));
+        return encode.getEncoded();
     }
 
     @Bean
     public MapReactiveUserDetailsService userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user")
-                .roles("USER")
+                .username("seller")
+                .password("seller")
+                .roles("SELLER")
                 .build();
         return new MapReactiveUserDetailsService(user);
     }
